49ca31fcec3d9b31050d478820e30d98b030c49b
JinSofty <144682207+JinSofty@users.noreply.github.com>
2023-10-31 19:40:51 +0900

    splash lecture

---
+++ lib/screen/spash/s_splash.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplshScreen extends StatefulWidget {
+  const SplshScreen({super.key});
+
+  @override
+  State<SplshScreen> createState() => _SplshScreenState();
+}
+
+class _SplshScreenState extends State<SplshScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
