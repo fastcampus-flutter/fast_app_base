8e771878c583114188feaec345073d8b0bba997b
JinSofty <144682207+JinSofty@users.noreply.github.com>
2023-10-31 21:48:05 +0900

    BottomNavigation logic update

---
+++ lib/screen/main/tab/ttoss/f_ttoss.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TtossFragment extends StatefulWidget {
+  const TtossFragment({super.key});
+
+  @override
+  State<TtossFragment> createState() => _TtossFragmentState();
+}
+
+class _TtossFragmentState extends State<TtossFragment> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
