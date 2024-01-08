7c897e94d9d79e060478c68bf5c5341bd9f5c752
윤종경 <jkyoon@danawa.com>
2024-01-08 15:05:35 +0900

    assets 이미지 추가, bank 더미데이터 추가

---
+++ lib/screen/main/tab/home/banks_dummy.dart
@@ -0,0 +1,6 @@
+import "package:fast_app_base/common/common.dart";
+import 'package:fast_app_base/screen/main/tab/home/vo/vo_bank.dart';
+
+final bankShinhan = Bank("신한은행", "$basePath/bank_shinban.png");
+final bankKakao = Bank("카카오뱅크", "$basePath/bank_kakao.png");
+final bankTtoss = Bank("토스뱅크", "$basePath/bank_toss.png");
