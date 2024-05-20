c0da28a3546f2fffe3d391eb115745318a9fb773
mimo626 <yessuk5499@gmail.com>
2024-05-20 11:54:25 +0900

    [feat] splash 화면 delay 설정

--- lib/screen/main/s_main.dart
+++ lib/screen/main/s_main.dart
@@ -1,7 +1,9 @@
+import 'package:fast_app_base/common/cli_common.dart';
 import 'package:fast_app_base/screen/main/tab/tab_item.dart';
 import 'package:fast_app_base/screen/main/tab/tab_navigator.dart';
 import 'package:flutter/material.dart';
-
+import 'package:after_layout/after_layout.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import '../../common/common.dart';
 import 'w_menu_drawer.dart';
 
@@ -12,7 +14,7 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => MainScreenState();
 }
 
-class MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin {
+class MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin, AfterLayoutMixin {
   TabItem _currentTab = TabItem.home;
   final tabs = [TabItem.home, TabItem.favorite];
   final List<GlobalKey<NavigatorState>> navigatorKeys = [];
@@ -24,6 +26,14 @@ class MainScreenState extends State<MainScreen> with SingleTickerProviderStateMi
   bool get extendBody => true;
 
   static double get bottomNavigationBarBorderRadius => 30.0;
+
+  // preserve한 splash를 제거
+  @override
+  FutureOr<void> afterFirstLayout(BuildContext context) {
+    delay((){
+      FlutterNativeSplash.remove();
+    }, 1500.ms);
+  }
 
   @override
   void initState() {
